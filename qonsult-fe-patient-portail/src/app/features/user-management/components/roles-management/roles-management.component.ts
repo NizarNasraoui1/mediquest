c82e7fbfd5e7e26c31175ad145d8f083be094eb7
Nizar <nizar.al.nasraoui@gmail.com>
2024-06-12 20:08:15 +0200

    refactoring

--- qonsult-fe-patient-portail/src/app/features/user-management/components/roles-management/roles-management.component.ts
+++ qonsult-fe-patient-portail/src/app/features/user-management/components/roles-management/roles-management.component.ts
@@ -11,29 +11,7 @@ export class RolesManagementComponent implements OnInit {
     form;
     roles = [];
     groups = [];
-    // roles: any[] = [
-    //     { name: 'roleName1', id: '1' },
-    //     { name: 'roleName2', id: '2' },
-    //     { name: 'Production', id: '3' },
-    //     { name: 'Research', id: '4' }
-    // ];
-    // groups=[
-    //     {
-    //         id:"1",
-    //         groupId:"1",
-    //         groupName:"groupName",
-    //         roles:[{
-    //             id:'1',
-    //             name:"roleName1"
-    //         },
-    //         {
-    //             id:'2',
-    //             name:"roleName2"
-    //         }
-    //     ]
-    //     }
-    // ];
-    selectedRoles=[];
+    groupRoles= [];
 
     constructor(private fb:FormBuilder,private userManagementService:UserManagementService){}
 
@@ -45,35 +23,23 @@ export class RolesManagementComponent implements OnInit {
         this.userManagementService.getRoleManagement().subscribe((res)=>{
             this.roles = res.roles;
             this.groups = res.groups;
-            // for(let i=0;i<this.groups.length;i++){
-            //     this.selectedRoles[i] = this.roles.map((e)=>{
-            //         e["selected"]= false;
-            //         return e;
-            //     });
-            //     //this.selectedRoles = [...this.selectedRoles,this.roles];
-            // }
-            console.log(this.selectedRoles);
-                // group.roles.forEach((role)=>{
-
-                // })
                 this.initForm();
-            })
+            });
 
     }
 
     initForm(){
         this.groups.forEach((e) => {
-            let alreadySelectedRoles = [...e.roles];
+            let selectedRoles = [...e.roles];
             let group = {
                 id:e.id,
-                alreadySelectedRoles:alreadySelectedRoles
+                selectedRoles:selectedRoles
             }
-            this.selectedRoles=[...this.selectedRoles,group];
+            this.groupRoles=[...this.groupRoles,group];
         });
-        console.log(this.selectedRoles);
     }
 
     save(){
-        console.log(this.selectedRoles)
+        console.log(this.groupRoles);
     }
 }
